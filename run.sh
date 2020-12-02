2351fd184651d1f82f75921ca88083a00e88b4d1
tshuang <tshuang@biolegend.com>
2020-12-02 13:42:23 -0800

    add misc_args and fix output dir issue

--- run.sh
+++ run.sh
@@ -15,11 +15,13 @@ set +e
 
 eval "$(/code/dnapipe/bin/conda shell.bash hook)"
 
+MISC_ARGS="--qc --part1-only --n-cores 12 ${OTHER_ARGS}"
+
 set -exuo pipefail
 
 cd ${OUTDIR}
 
-tapestri dna run --output-folder ${OUTDIR} --output-prefix ${MB_SAMPNAME} \
+
+tapestri dna run --output-folder ${OUTDIR}/${MB_SAMPNAME} --output-prefix ${MB_SAMPNAME} \
   --r1 /data/input/Read1.fq.gz --r2 /data/input/Read2.fq.gz  \
-  --config ${CONFDIR}/conf.yaml \
-  --qc --part1-only --overwrite --n-cores 12 
+  --config ${CONFDIR}/conf.yaml ${MISC_ARGS}
